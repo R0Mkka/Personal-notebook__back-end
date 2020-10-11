327ebbae500d42505c65ba6f53a494af9b101a81
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2020-10-11 23:08:44 +0300

    test

--- server.js
+++ server.js
@@ -18,30 +18,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./routes')(app);
 
-app.get('/', (req, res) => {
-    res.send({
-        kek: 'test',
-    });
-});
+app.get('/', (_, res) => res.send('Welcome to Personal-notebook!'));
 
 app.listen(
     PORT,
     () => {
-        console.log(`App is listening on port: ${PORT}!`);
-        // mongoose.connect(
-        //     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@base-cluster.hdsjj.mongodb.net/${DB_NAME}`,
-        //     {
-        //         useNewUrlParser: true,
-        //         useUnifiedTopology: true,
-        //     },
-        // )
-        // .then(() => {
-        //     console.log('DB connected!');
-        //     console.log(`App is listening on port: ${PORT}!`);
-        // })
-        // .catch((error) => {
-        //     console.error('APP LAUNCHING ERROR:', error);
-        //     mongoose.disconnect();
-        // });
+        mongoose.connect(
+            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@base-cluster.hdsjj.mongodb.net/${DB_NAME}`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
+        )
+        .then(() => {
+            console.log('DB connected!');
+            console.log(`App is listening on port: ${PORT}!`);
+        })
+        .catch((error) => {
+            console.error('APP LAUNCHING ERROR:', error);
+            mongoose.disconnect();
+        });
     },
 );
